057d1baf0b404a622f900f6b35d58d7bb940fb4f
Stevanus Andrianta <sandrianta@gmail.com>
2016-06-02 16:50:00 +0700

    Create Perceptron.scala

---
+++ src/main/scala/Perceptron.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by stevanus.andrianta on 6/2/2016.
+  */
+
+case class Perceptron(perceptronId: String = "perceptron",
+                      var activation: Double = 0.00,
+                      var error: Double = 0.00,
+                      isBias: Boolean = false)
