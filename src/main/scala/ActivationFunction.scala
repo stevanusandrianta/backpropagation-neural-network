802ba4bda47e174e924a509b0f80e4564ed53e22
Stevanus Andrianta <sandrianta@gmail.com>
2016-06-02 18:07:37 +0700

    Update ActivationFunction.scala
    change activation function into trait

--- src/main/scala/ActivationFunction.scala
+++ src/main/scala/ActivationFunction.scala
@@ -2,23 +2,19 @@
   * Created by stevanus.andrianta on 6/2/2016.
   */
 
-case class ActivationFunction(option: String = "sigmoid"){
+trait ActivationFunctionTrait{
+  def activate(z: Double) : Double
+}
 
-  def sigmoid(z: Double): Double = {
+object Sigmoid extends ActivationFunctionTrait{
+  override def activate(z:Double) : Double = {
     1 / (1 + Math.exp(z * (-1)))
   }
+}
 
-  def step(z: Double): Double = {
+object Step extends ActivationFunctionTrait{
+  override def activate(z:Double) : Double = {
     if(z<0) 0
     else 1
   }
-
-  def activate(z: Double): Double = {
-    option match {
-      case "sigmoid" => sigmoid(z)
-      case "step" => step(z)
-      case _ => sigmoid(z)
-    }
-  }
-
 }
